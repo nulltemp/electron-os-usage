b1b50327ed9ca8cc29f2cdba7cbd8ead82ecca94
nulltemp <nulltemp999@gmail.com>
2018-08-07 12:57:03 +0900

    メニューバーを修正し、バージョン情報はヘルプから表示させるように修正

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
- 
+const Menu = electron.Menu;
+
 let mainWindow;
- 
+
+let template = [
+  {
+    label: 'ファイル',
+    submenu: [
+      {
+        label: '終了',
+        role: 'close'
+      }
+    ]
+  }, {
+    label: 'ヘルプ',
+    submenu: [
+      {
+        label: 'バージョン',
+        click: function () {
+          const messageVal = 'node ' + process.versions.node + ', Chrome ' + process.versions.chrome + ', Electron ' + process.versions.electron + '.';
+
+          electron.dialog.showMessageBox(mainWindow, {type: 'none', title: 'version', message: messageVal});
+        }
+      }
+    ]
+  }
+];
+
 function createWindow() {
+  const newMenu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(newMenu);
+
   mainWindow = new BrowserWindow({width: 800, height: 600});
  
   mainWindow.loadFile('index.html');
@@ -13,7 +41,7 @@ function createWindow() {
     mainWindow = null;
   });
 }
- 
+
 app.on('ready', createWindow);
  
 app.on('window-all-closed', () => {
